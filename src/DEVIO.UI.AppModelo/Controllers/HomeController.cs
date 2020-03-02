37fc72340c8ad22efd66638299dba9e521f572ef
Lucas Simões <lucassimoes_@hotmail.com>
2020-03-01 22:43:11 -0300

    Final Criando um projeto MVC sem template

---
+++ src/DEVIO.UI.AppModelo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEVIO.UI.AppModelo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
